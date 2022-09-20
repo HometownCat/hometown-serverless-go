19da04d2bb1a4fbd04bbbd28f3cfdc75b0cb2bee
PARKNAMSU <skatn7979@gmail.com>
2022-09-20 11:23:25 +0900

    common

--- modules/common/common.go
+++ modules/common/common.go
@@ -1,5 +1,27 @@
 package common
 
+import (
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
 func IsError(err error) bool {
 	return err != nil
+}
+
+func ResponseError(err error) (events.APIGatewayProxyResponse, error){
+	errBin, err := json.Marshal(err)
+
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			Body: "json parsing error",
+			StatusCode: 400,
+		},err
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body: string(errBin),
+		StatusCode: 400,
+	},err
 }
